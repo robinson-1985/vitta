b815a12239861f950f2d8aadecb1fde52b67b215
robinson <robinpaulista85@gmail.com>
2026-01-21 17:52:25 -0300

    Configure production settings for Railway deployment

--- vitta/settings.py
+++ vitta/settings.py
@@ -130,7 +130,7 @@ TIME_FORMAT = 'H:i'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
