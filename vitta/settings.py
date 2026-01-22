f3a1f4669f74fb64042287eb327ddeb9bc76ae45
robinson <robinpaulista85@gmail.com>
2026-01-22 20:36:15 -0300

    Remove automatic superuser creation

--- vitta/settings.py
+++ vitta/settings.py
@@ -12,19 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import dj_database_url
 from pathlib import Path
-
 import os
-
-if os.environ.get("RAILWAY_ENVIRONMENT") == "production":
-    from django.contrib.auth import get_user_model
-
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@vitta.com",
-            password="SenhaForte123"
-        )
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
